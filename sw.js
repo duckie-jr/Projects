a04cb2053fbad768c710050234aeb0acf0c27566
duckie-jr <0sigma.balls0@gmail.com>
2026-06-02 11:24:29 -0500

    Update asset paths to use relative URLs

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "duckie-v1";
-const ASSETS = ["/", "/index.html", "/style.css", "/main.js", "/favicon.svg"];
+const ASSETS = ["./", "./index.html", "./style.css", "./main.js", "./favicon.svg"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
